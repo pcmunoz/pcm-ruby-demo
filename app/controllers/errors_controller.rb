e741c43671c21e14e621b170518ac996cd3bc8c3
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2019-07-11 00:21:27 +0800

    Update error handling

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,24 @@
 class ErrorsController < ApplicationController
     def server_error
-        @status_code = params[:status_code]
+        @error = error_params(params[:status_code])
         render
+    end
+
+    private
+
+    def error_params(code)
+        result = {:status_code => code}
+        case code.to_i
+        when 404
+            result[:title] = "message 404"
+            result[:message] = "message 404"
+        when 503
+            result[:title] = "message 503"
+            result[:message] = "message 404"
+        else
+            result[:title] = "message default"
+            result[:message] = "message 404"
+        end
+        result
     end
 end
